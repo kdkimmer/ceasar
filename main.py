ae803500d6386dfd699cea57ac65e7ff20d47fce
Kim Druessel <sageykat-123@yahoo.com>
2016-08-25 13:39:55 -0500

    finished Caesar

--- main.py
+++ main.py
@@ -15,11 +15,77 @@
 # limitations under the License.
 #
 import webapp2
-int 12;
+from caesar import encrypt
+import cgi
+
+# html boilerplate for the top of every page
+page_header = """
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+            p.error {
+                color: red;
+            }
+        </style>
+        <title>Caesar</title> 
+    </head>
+<body>
+    <h1>
+        <a href="/">Caesar</a>
+    </h1>
+    """
+
+input_form = """
+    	<form action= /rotate method="post">
+    			<div>
+        		<label for="rot">Rotate by how many (must be an int):</label>
+                <input type="number" name="rot"value="{0}">
+                <p class="error"></p>
+                </div>
+            	<textarea type="text" name="input_text_area">{1}</textarea>
+            	<br>
+            	<input type="submit" value="Encrypt it" name="submit-button">
+    </form>
+    """
+    
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+    	
+    	self.response.write(page_header + input_form.format('0','') + page_footer)
+
+
+class RotateHandler(webapp2.RequestHandler):
+
+   	def post(self):
+    		rotate = self.request.get("rot")
+    		rot = int(rotate)
+    		user_text = self.request.get("input_text_area")
+    		rotate_text = encrypt(user_text, rot)
+    		escape_text = cgi.escape(rotate_text)
+    		
+        	rotate_answer = page_header + input_form.format(rot, escape_text) + page_footer
+    		self.response.write(rotate_answer)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/rotate', RotateHandler)
 ], debug=True)
